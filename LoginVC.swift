c511fb5225e9e0b79a20e4944e7df21b79d5c013
Dmytro <dmytro.fedorov@icloud.com>
2016-09-20 14:45:26 +0400

    AddedViewControllers)
    Have named them, changed class name and now I’m happy !)

---
+++ LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Dmytro Fedorov on 9/20/16.
+//  Copyright © 2016 Dmytro Fedorov. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+
+
+}
+
